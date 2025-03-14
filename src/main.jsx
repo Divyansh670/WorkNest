aceb94a5a5ad580c662a46ff8ad628a92c1b7f91
Divyansh1805 <sridivyansh03@gmail.com>
2025-03-14 23:42:00 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // Ensure styles are imported
+import TaskContext from "./context/TaskContext";
+import AuthContext from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  
-    <App />
+    <AuthContext>
+         <TaskContext>
+     <App />
+  </TaskContext>
+    </AuthContext>
+ 
+   
 );
