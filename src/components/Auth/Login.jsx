533d203ffc4b52f322d98b9a2064b4fb7166da17
Divyansh1805 <sridivyansh03@gmail.com>
2025-03-17 01:53:29 +0530

    last

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -27,17 +27,17 @@ const Login = ({ handleLogin }) => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
-            className="outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400"
+            className="outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-10 rounded-full placeholder:text-gray-400"
             type="email"
-            placeholder="Enter your email"
+            placeholder="e@e.com or admin@me.com"
           />
           <input
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
-            className="outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400"
+            className="outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-10 rounded-full mt-3 placeholder:text-gray-400"
             type="password"
-            placeholder="Enter password"
+            placeholder="try '123'"
           />
           <button
             className="mt-7 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full placeholder:text-white"
